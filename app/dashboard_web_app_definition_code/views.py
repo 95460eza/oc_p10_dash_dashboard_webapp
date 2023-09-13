43c6b1c61d3ad182e469144e6093667157574858
95460eza <alfredkoffisokpa@gmail.com>
2023-09-13 19:06:50 -0400

    debug

--- app/dashboard_web_app_definition_code/views.py
+++ app/dashboard_web_app_definition_code/views.py
@@ -15,45 +15,14 @@ external_stylesheets = [dmc.theme.DEFAULT_COLORS]
 app6 = Dash(__name__, external_stylesheets=external_stylesheets)
 
 
-data_directory = os.path.join( os.getcwd(), "app/dashboard_web_app_definition_code/dash_texts_data" )
-file_to_load  = os.path.join(data_directory,"df_airline_tweets.csv")
-# Read in Data
-#df = pd.read_csv('./dash_texts_data/df_airline_tweets.csv')[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
-
-#df = pd.read_csv("dash_texts_data/df_airline_tweets.csv")[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
-df = pd.read_csv(file_to_load)[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
-
-
-# CALLBACK Enables the use of control components for building the interaction
-@callback(
-    # You must provide all Outputs first. "component_property" are the parameters whose values the interaction will change
-    Output(component_id='graph-to-display', component_property='figure'),
-    Input(component_id='radio-button-to-select-column', component_property='value'),
-
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='airline', y=col_chosen, histfunc='avg')
-    return fig
 
 
 # Define the Web App Layout
-app6.layout = dbc.Container([dmc.Title('Welcome DATA Apprentice', color="blue", size="h3"),
-                             dmc.RadioGroup([dmc.Radio(i, value=i) for i in
-                                             ['airline_sentiment_confidence', 'negativereason_confidence',
-                                              'retweet_count']], id='radio-button-to-select-column',
-                                            value='retweet_count', size="sm"),
-                             dmc.Grid([
-                                 dmc.Col([dash_table.DataTable(data=df.to_dict('records'), page_size=12,
-                                                               style_table={'overflowX': 'auto'})], span=6),
-                                 dmc.Col([dcc.Graph(figure={}, id='graph-to-display')], span=6)
+app6.layout = html.Div([
 
-                             ]
-
-                             ),  # "," not needed
-
-                             ], fluid=True
-
-                            )
+    # The html.Div() (note the ABSENCE of []) DASH COMPONENT with children= parameter adds TEXT to the webpage
+    html.Div(children='Welcome DATA Apprentice'),
+    ])
 
 #if __name__ == '__main__':
 #    app6.run(debug=True)
