1fcaad24596aaaa044ef472d685331d881ce55a8
95460eza <alfredkoffisokpa@gmail.com>
2023-09-13 17:23:48 -0400

    use os.path.join()

--- app/dashboard_web_app_definition_code/views.py
+++ app/dashboard_web_app_definition_code/views.py
@@ -3,7 +3,8 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import dash_bootstrap_components as dbc  # To define rows and columns on the page
 import dash_mantine_components as dmc  # To define a grid on the page within which to insert dmc.Cols and define their width by assigning a number to the span property.
 import pandas as pd
-import plotly.express as px
+import plotly.express as
+import os
 
 
 # Creates the DASH INTERACTIVE web app OBJECT (content is interactive and will be seen in an browser)
@@ -13,11 +14,14 @@ import plotly.express as px
 external_stylesheets = [dmc.theme.DEFAULT_COLORS]
 app6 = Dash(__name__, external_stylesheets=external_stylesheets)
 
+
+data_directory = os.path.join( os.getcwd(), "app/dashboard_web_app_definition_code/dash_texts_data" )
+file_to_load  = os.path.join(data_directory,"df_airline_tweets.csv")
 # Read in Data
 #df = pd.read_csv('./dash_texts_data/df_airline_tweets.csv')[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
 
 #df = pd.read_csv("dash_texts_data/df_airline_tweets.csv")[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
-df = pd.read_csv("df_airline_tweets.csv")[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
+df = pd.read_csv(file_to_load)[ ['airline', 'airline_sentiment_confidence', 'negativereason_confidence', 'retweet_count']]
 
 
 # CALLBACK Enables the use of control components for building the interaction
